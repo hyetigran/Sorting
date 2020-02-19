f51dc251ec0d9eee3ff3b3b8b79d35a622b90593
hyetigran <tigran.asriyan@gmail.com>
2020-02-18 21:20:15 -0500

    complete merge sort and pass test

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,16 +1,46 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+def merge(arr, arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+
+    left_counter = 0
+    right_counter = 0
+    sorted_counter = 0
+
+    while left_counter < len(arrA) and right_counter < len(arrB):
+        if arrA[left_counter] < arrB[right_counter]:
+            arr[sorted_counter] = arrA[left_counter]
+            left_counter += 1
+        else:
+            arr[sorted_counter] = arrB[right_counter]
+            right_counter += 1
+        sorted_counter += 1
+
+    while left_counter < len(arrA):
+        arr[sorted_counter] = arrA[left_counter]
+        left_counter += 1
+        sorted_counter += 1
+
+    while right_counter < len(arrB):
+        arr[sorted_counter] = arrB[right_counter]
+        right_counter += 1
+        sorted_counter += 1
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+def merge_sort(arr):
+    middle = (arr[0] + (len[arr]-1))//2
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
 
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        merge(arr, left_arr, right_arr)
     return arr
 
 
@@ -20,7 +50,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +59,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
